78fd268966e776d48e63966f284f4f45c83359c9
MarinaGregorini <marinasgregorini@gmail.com>
2025-03-04 11:55:22 +0000

    versões finais

--- forca.py
+++ forca.py
@@ -1,17 +1,15 @@
-from desenharforca import desenhar_forca
-
 def escolher_letra():
-    input_letra =(input("Tenta uma letra: ")).lower()
-    return input_letra
+    while True:
+        input_letra =(input("Tenta uma letra: ")).lower()
+        if input_letra.isalpha() and len(input_letra) == 1:
+            return input_letra
+        else:
+            print("Entrada inválida.")
+            continue
 
-tentativa = 0
 def verificar_letra(letras_palavra, input_letra):
-    global tentativa
     if input_letra in letras_palavra:
-        return input_letra
+        return True
     else:
-        tentativa += 1
         print(f"A letra {input_letra} não faz parte desta palavra.")
-        desenhar_forca(tentativa)
-        print(tentativa)
-        return tentativa
+        return False
--- listadepalavras.py
+++ listadepalavras.py
@@ -1,9 +1,7 @@
-
 import random
 
-# Função para escolher uma palavra aleatória
 def escolher_palavra():
     palavras = ["panda", "urso", "elefante", "macaco"]
-    palavra_aleatoria = random.choice(palavras)  # Escolhe uma palavra aleatória da lista
+    palavra_aleatoria = random.choice(palavras)  
     letras_palavra = list(palavra_aleatoria)
-    return letras_palavra  # Retorna as letras separadas da palavra escolhida
+    return letras_palavra
