3ecd2c1bf4257cf283f6869b3c0b19711e806a7c
MarinaGregorini <marinasgregorini@gmail.com>
2025-03-04 11:08:20 +0000

    jogo corre bem, mas a var tentativa não está atualizando em jogo

--- imprimir_palavra.py
+++ imprimir_palavra.py
@@ -1,19 +1,7 @@
-def imprimir_palavra(letras_palavra, input_letra=None):
-    global impressao_passada
-    if input_letra == None:
-        if impressao_passada == None:
-            for letra in letras_palavra:
-                print("_", end=" ")
-        else:
-            for letra in impressao_passada:
-                print(letra, end=" ")
-    else:
-        for letra in letras_palavra:
+def imprimir_palavra(letras_palavra, impressao_passada, input_letra=None):
+    if input_letra is not None:
+        for i, letra in enumerate(letras_palavra):
             if letra == input_letra:
-                impressao_passada.append(letra)
-                print(letra, end=" ")
-            elif letra != input_letra:
-                impressao_passada.append("_")
-                print("_", end=" ")
-        print(impressao_passada)
+                impressao_passada[i] = letra 
                 
+    print(" ".join(impressao_passada)) 
--- jogo.py
+++ jogo.py
@@ -1,12 +1,28 @@
 from imprimir_palavra import imprimir_palavra
-from forca import escolher_letra, veriificar_letra
+from forca import escolher_letra, verificar_letra, tentativa
 from listadepalavras import escolher_palavra
 
-tentativa = 0
-while tentativa < 6:
-    escolher_letra()
+print("Jogo da forca.")
+
+letras_palavra = escolher_palavra()
+
+impressao_passada = ["_"] * len(letras_palavra)
+print(" ".join("_" for _ in impressao_passada))
+
+while tentativa < 6 and "_" in impressao_passada:
     
-    escolher_palavra()
-    imprimir_palavra(letras_palavra, input_letra)
+    input_letra = escolher_letra()
+    
+    verificar_letra(letras_palavra, input_letra)
+    
+    imprimir_palavra(letras_palavra, impressao_passada, input_letra)
+    
+if "_" not in impressao_passada:
+    print("Parabéns!")
+
+elif tentativa >= 6:
+    print("RIP") 
+    
+    
     
     
